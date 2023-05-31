1a5a9199876117c3339ea2a880aaad567097948b
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-06-01 00:43:14 +0900

    追加

--- regex.js
+++ regex.js
@@ -9,12 +9,14 @@ console.log(target.match(regex)); // => [ 'JavaScript', 'React', 'TypeScript' ]
 
 
 const target2 = 'a c z # % @ b';
-
 // [acz#]は、a, c, z, #のどれか1文字にマッチする正規表現です。
 const regex2 = /[acz#]/g;
-
 // matchメソッドを用いることで、文字列中から正規表現にマッチする文字列を取得できます。
 console.log(target2.match(regex2));
+
+const target3 = 'grey gray';
+const regex3 = /gr[ae]y/g;
+console.log(target3.match(regex3));
 
 
 
