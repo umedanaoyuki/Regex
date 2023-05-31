f8e967ea65110230c1df26ea12a92222de83f609
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-06-01 00:40:31 +0900

    追加

--- regex.js
+++ regex.js
@@ -1,4 +1,4 @@
-正規表現まとめ
+//正規表現まとめ
 
 // 正規表現を適用させる文字列
 const target = 'JavaScript and React are different. TypeScript is a great language.';
@@ -6,5 +6,15 @@ const target = 'JavaScript and React are different. TypeScript is a great langua
 const regex = /[A-Z][a-zA-Z]+/g;
 // matchメソッドを用いることで、文字列中から正規表現にマッチする文字列を取得できます。
 console.log(target.match(regex)); // => [ 'JavaScript', 'React', 'TypeScript' ]
+
+
+const target2 = 'a c z # % @ b';
+
+// [acz#]は、a, c, z, #のどれか1文字にマッチする正規表現です。
+const regex2 = /[acz#]/g;
+
+// matchメソッドを用いることで、文字列中から正規表現にマッチする文字列を取得できます。
+console.log(target2.match(regex2));
+
 
 
